cc2a472c565a3381a8d6e7193dfcdc8dc255c019
cthomas265 <98835210+cthomas265@users.noreply.github.com>
2022-03-18 18:30:35 -0500

    funtions and const

--- Develop/script.js
+++ Develop/script.js
@@ -1,17 +1,36 @@
 // Assignment code here
+//Generator functions - https://www.net-comber.com/charset.html
 
+const resultEl = document.getElementById('result');
+const lengthEl = document.getElementById('length');
+const uppercaseEl = document.getElementById('uppercase');
+const lowercaseEl = document.getElementById('lowercase');
+const numberEl = document.getElementById('result');
+const symbolEl = document.getElementById('result');
+const generateEl = document.getElementById('result');
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbols,
 }
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function getRandomLower () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+function getRandomUpper () {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+function getRandomNumber () {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+function getRandomSymbols () {
+  const symbols = '/*-+.?<>;:"|{[}]!@#%$^&'
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
+
+console.log(getRandomLower())
